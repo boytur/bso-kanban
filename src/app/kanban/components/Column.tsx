bd5b55de65f48ec79533017233cefa0f4dd13a42
Jessada.n <Jessada.n@clicknext.com>
2024-11-11 18:38:49 +0700

    Fix - and optimize Kanban Column component UI and logic

--- src/app/kanban/components/Column.tsx
+++ src/app/kanban/components/Column.tsx
@@ -53,17 +53,17 @@ const Column: React.FC<ColumnProps> = ({ columnId, column, index, insertTask, se
                     id: `${Date.now()}`, // สร้าง id ใหม่ตามเวลาปัจจุบัน หรืออาจสร้างแบบอื่นที่เหมาะสม
                     content: inputRef.current.value,
                 };
-    
+
                 setData({ ...data, [columnId]: { name: column.name, items: [...column.items, newTask] } })
-    
+
                 // เรียก insertTask เพื่ออัปเดตข้อมูลในฐานข้อมูลหรือ backend
-                insertTask(inputRef.current.value, columnId , data[columnId].items.length);
-    
+                insertTask(inputRef.current.value, columnId, data[columnId].items.length);
+
                 inputRef.current.value = '';
             }
-    
+
         } catch (error) {
-            
+
         }
     }
 
@@ -76,10 +76,10 @@ const Column: React.FC<ColumnProps> = ({ columnId, column, index, insertTask, se
                         {...provided.draggableProps}
                         className="flex flex-col w-64 shadow-md bg-white border border-gray-300"
                     >
-                        <h2 {...provided.dragHandleProps} className="text-lg font-bold mb-4 bg-white shadow-md sticky top-0 z-10 px-4 py-2">
+                        <h2 {...provided.dragHandleProps} className="text-lg font-bold bg-white shadow-md sticky top-0 z-10 px-4 py-2">
                             {column.name}
                         </h2>
-                        <div className='px-4'>
+                        <div className='px-4 max-h-[670px] overflow-y-auto p-2'>
                             <Droppable droppableId={columnId} type="item" direction='vertical' isDropDisabled={false} isCombineEnabled={false} ignoreContainerClipping={false}>
                                 {(provided, snapshot) => (
                                     <div
@@ -89,7 +89,7 @@ const Column: React.FC<ColumnProps> = ({ columnId, column, index, insertTask, se
                                     >
                                         {column.items.map((item, index) => (
                                             <DraggableItem key={item.id} item={item} index={index} />
-                                            ))}
+                                        ))}
                                         {provided.placeholder}
                                         {!column.items.length && (
                                             <p className="text-center text-gray-500 mt-4">ไม่มีไอเท็มในคอลัมน์นี้</p>
@@ -97,12 +97,7 @@ const Column: React.FC<ColumnProps> = ({ columnId, column, index, insertTask, se
                                     </div>
                                 )}
                             </Droppable>
-                            <button
-                                onClick={() => setIsAddTaskOpen(true)}
-                                className={`${isAddTaskOpen ? 'hidden' : 'block'} text-teal-500 opacity-0 hover:opacity-100 transition-opacity duration-300 hover:pointer-events-auto border-2 border-teal-500 rounded-md w-full px-2 py-1`}
-                            >
-                                เพิ่ม
-                            </button>
+
                             <div
                                 ref={addTaskRef}
                                 className={`${isAddTaskOpen ? 'block' : 'hidden'} bg-white p-0`}
@@ -110,6 +105,12 @@ const Column: React.FC<ColumnProps> = ({ columnId, column, index, insertTask, se
                                 <input type="text" className="w-full p-2 border border-gray-300 rounded-md" ref={inputRef} />
                             </div>
                         </div>
+                        <button
+                            onClick={() => setIsAddTaskOpen(true)}
+                            className={`${isAddTaskOpen ? 'hidden' : 'block'} text-teal-500 opacity-0 hover:opacity-100 transition-opacity duration-300 hover:pointer-events-auto border-2 border-teal-500 rounded-md w-full px-2 py-1`}
+                        >
+                            เพิ่ม
+                        </button>   
                     </div>
                 )}
             </Draggable>
